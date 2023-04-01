96a7df33226400f65e042fdc07c19071b42bd5f2
abidkhan <abidkhan4357@gmail.com>
2023-04-01 12:33:27 -0400

    set headless to true and update github yml file to run in container with cache

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    headless: false,
+    headless: true,
     storageState: "./loginAuth.json",
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
